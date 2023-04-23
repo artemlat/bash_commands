bbe6cbf1bfa5d2ecaa6d171025341beeb05769f3
artemlat <artemlat386@gmail.com>
2023-04-23 23:52:19 +0300

    commiting script

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bashi
+
+cd linux_terminal
+mkdir script_folder1 script_folder2 script_folder3
+cd script_folder1
+touch script_file1.txt script_file2.txt script_file3.txt script_file4.json script_file5.json
+mkdir 1scripted 2scripted 3scripted
+ls -la
+mv /d/лекции по учебе/34 группа/linux_terminal/script_folder1/script_file1.txt d/лекции по учебе/34 группа/linux_terminal/script_folder2
